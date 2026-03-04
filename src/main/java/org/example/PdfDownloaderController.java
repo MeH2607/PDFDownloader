602589ad313f7aa3ee28832ccfa402f0a2a1d89b
Mohamed El Haddaoui <99melh@gmail.com>
2026-03-04 16:03:07 +0100

    added getmapping for downloadstatuses

--- src/main/java/org/example/PdfDownloaderController.java
+++ src/main/java/org/example/PdfDownloaderController.java
@@ -10,9 +10,13 @@ import org.apache.hc.core5.http.io.entity.EntityUtils;
 import org.example.entities.DownloadStatus;
 import org.example.entities.ExcelRow;
 import org.example.excel.ApachePoiExcelReader;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -64,20 +68,27 @@ public class PdfDownloaderController {
         return downloadStatusList;
     }
 
+
     @PostMapping("/pdf/test-from-local-excel")
     public ResponseEntity<List<DownloadStatus>> testFromLocalExcel() throws Exception {
         String excelPath = "src/GRI_2017_2025_test.xlsx";
         return ResponseEntity.ok(downloadPdfs(excelPath));
     }
 
-    /*@PostMapping(value = "/pdf/upload-excel", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @PostMapping(value = "/pdf/upload-excel", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
 public ResponseEntity<List<DownloadStatus>> uploadExcel(@RequestParam("file") MultipartFile file) throws Exception {
     Path temp = Files.createTempFile("upload-", ".xlsx");
     file.transferTo(temp.toFile());
     try {
-        return ResponseEntity.ok(yourService.downloadPdfs(temp.toString()));
+        return ResponseEntity.ok(downloadPdfs(temp.toString()));
     } finally {
         Files.deleteIfExists(temp);
     }
-}*/
+}
+
+    @GetMapping("/pdf/getStatusList")
+    public ResponseEntity<List<DownloadStatus>> getAllDownloadStatuses(List<DownloadStatus> downloadStatusList){
+
+        return ResponseEntity.ok(downloadStatusList);
+    }
 }
