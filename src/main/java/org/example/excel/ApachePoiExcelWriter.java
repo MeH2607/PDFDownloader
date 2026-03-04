b6e25f501cd63d330248aabfb986ffb9feace450
Mohamed El Haddaoui <99melh@gmail.com>
2026-03-04 10:09:16 +0100

    Updated ExcelWriter interface to use List<DownloadStatus>

--- src/main/java/org/example/excel/ApachePoiExcelWriter.java
+++ src/main/java/org/example/excel/ApachePoiExcelWriter.java
@@ -1,11 +1,12 @@
 package org.example.excel;
 
+import org.example.entities.DownloadStatus;
 import org.example.entities.ExcelRow;
 
 import java.util.List;
 
 public class ApachePoiExcelWriter implements ExcelWriter {
-    public void write(String filePath, List<ExcelRow> rows) {
+    public void write(String filePath, List<DownloadStatus> downloadList) {
         // use Apache POI to write Excel
     }
 }
--- src/main/java/org/example/excel/ExcelWriter.java
+++ src/main/java/org/example/excel/ExcelWriter.java
@@ -1,9 +1,10 @@
 package org.example.excel;
 
+import org.example.entities.DownloadStatus;
 import org.example.entities.ExcelRow;
 
 import java.util.List;
 
 public interface ExcelWriter {
-    void write(String filePath, List<ExcelRow> rows);
+    void write(String filePath, List<DownloadStatus> downloadList);
 }
